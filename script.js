6b8f92cef23db6cc3fc0a0c701836991fea8a056
moony404 <theo.marliac63@gmail.com>
2024-12-26 18:59:34 +0100

    update save1 25_12_2024 11_40

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('%c Hello World ! ', 'background: #222; color: #bada55');
+
+// gif part for the down arrow
+// document.addEventListener("DOMContentLoaded", function() {
+//   const scrollGIF = document.getElementById("scrollGif");
+  
+//   window.addEventListener("scroll", () => {
+//       const scrollPosition = window.scrollY;
+//       const maxScroll = 500;
+//       let opacity = 1 - scrollPosition / maxScroll;
+//       if (opacity < 0) opacity = 0;
+//       scrollGIF.style.opacity = opacity;
+//   });
+// });
+
+
+// this part up is bugged, pi**** what have you donne
